5d21849ff480582813f5222e028618997b9bd564
Eric Diviney <etdiviney14@yahoo.com>
2014-04-17 20:59:02 -0500

    Initial Website
    Documentation update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+
+var el = document.getElementById("dropdown");
+var body = document.body;
+
+
+
+el.onmouseover = function()
+{
+	var list = document.getElementById("dropdown-menu");
+	list.style.display = "block"
+};
+
+el.onmouseout = function()
+{
+	var list = document.getElementById("dropdown-menu");
+	list.style.display = "none"
+};
